951eccf071ec53eeea3537571447c87643490b3d
shahanaan17-png <shahanaan17@gmail.com>
2026-08-06 10:57:47 +0530

    Time: 0 ms (100%), Space: 54.2 MB (16.26%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+    
+};
+console.log(toLowerCase("Hello"));
+console.log(toLowerCase("here"));
+console.log(toLowerCase("LOVELY"));
